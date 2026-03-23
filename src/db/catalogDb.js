165344ad02213290c497b4e1e4de8e7df2851998
Volodymyr-Kushnir27 <volodumur.kushnir@gmail.com>
2026-03-23 19:55:38 +0200

    БД 4 нові арома

--- src/db/catalogDb.js
+++ src/db/catalogDb.js
@@ -1,6 +1,13 @@
 const Database = require("better-sqlite3");
-const { DB_PATH } = require("../config");
 
-const db = new Database(DB_PATH, { readonly: true });
+const DB_PATH =
+  process.env.CATALOG_DB_PATH ||
+  (process.env.DATA_DIR
+    ? process.env.DATA_DIR + "/perfumes.sqlite"
+    : "./data/perfumes.sqlite");
+
+console.log("📦 DB PATH:", DB_PATH);
+
+const db = new Database(DB_PATH);
 
 module.exports = db;
